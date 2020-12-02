573f0a38d01d4279187b7dced76ba22123d98c26
Adriano Mariani <adriano.mariani@ryerson.ca>
2020-12-02 01:27:57 -0500

    added cors

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, request, Response
 from search import Search
+from flask_cors import CORS
 import json
 
 app = Flask(__name__)
+CORS(app)
 search = Search('crawler/pages.json')
 
 @app.route('/query', methods=['POST'])
 def query():
     resp = Response(json.dumps(search.query(request.get_json()['query'])))
-    resp.headers["Content-Type"] = 'text/json'
+    resp.headers["Content-Type"] = 'application/json'
     return resp
